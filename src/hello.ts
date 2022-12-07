cd2b69ee391aa1400cd69abedbca162c51164ae0
Matthew Goff <75225244+matthew-goff-dot-com@users.noreply.github.com>
2022-12-07 11:10:10 -0600

    PLAT-156: Set up prototype for serverless REST API Gateway.

---
+++ src/hello.ts
@@ -0,0 +1,8 @@
+module.exports.handler = async (event) => {
+    // TODO implement
+    // TODO Make it so the lambdas don't end in -index. Please.
+    return {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+}
